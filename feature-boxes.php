84b1d2438ad8ed43fbd17c44badd580a9f3e6a98
wikitopian <matt@swarmstrategies.com>
2013-11-20 05:32:37 +0000

    init

---
+++ feature-boxes.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name: Feature Boxes
+ * Plugin URI:  http://www.github.com/wikitopian/feature-boxes
+ * Description: Shortcode-activated boxes
+ * Version:     0.1.0
+ * Author:      @wikitopian
+ * Author URI:  http://www.github.com/wikitopian
+ * License:     DWTFYWT
+ */
+
+class Feature_Boxes {
+	public function __construct() {
+	}
+}
+$feature_boxes = new Feature_Boxes();
